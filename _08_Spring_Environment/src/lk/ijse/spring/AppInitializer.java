4c2b8f2134e4486ab01320f408aadeb60b468845
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-13 15:53:25 +0530

    Update Content

--- _08_Spring_Environment/src/lk/ijse/spring/AppInitializer.java
+++ _08_Spring_Environment/src/lk/ijse/spring/AppInitializer.java
@@ -1,12 +1,33 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Map;
+import java.util.Properties;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
-        ctx.register();
+        ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
+
+
+        //OS environment variables
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            String value = getenv.get(key);
+            System.out.println(key+" : "+value);
+        }
+
+        System.out.println("===============");
+
+        //Java Environment Variables
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            Object value = properties.get(key);
+            System.out.println(key+" : "+value);
+        }
     }
 }
