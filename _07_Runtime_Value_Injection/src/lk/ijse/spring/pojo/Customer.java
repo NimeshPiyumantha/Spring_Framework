aefac49dccd56ff65662e9d24075cee27b4fa040
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-13 15:22:56 +0530

    Add Spring_Configurations  @Autowired

--- _07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
+++ _07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
@@ -1,10 +1,18 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
+
+    @Autowired
+    public Customer(@Value("IJSE") String name){
+        System.out.println("Customer:Instantiated " +name);
+    }
+
 
     public Customer(@Value("Nimesh,Akila,Pasan") String names[]){
         System.out.println("Customer:Instantiated ");
