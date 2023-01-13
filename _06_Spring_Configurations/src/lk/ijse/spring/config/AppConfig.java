ac54048f411e1980cb9a3da28d68737f88d419cc
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-13 14:50:27 +0530

    Update Content

--- _06_Spring_Configurations/src/lk/ijse/spring/config/AppConfig.java
+++ _06_Spring_Configurations/src/lk/ijse/spring/config/AppConfig.java
@@ -2,9 +2,14 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring")
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
+@Import({ConfigOne.class,ConfigTwo.class})
+//@ImportResource("classpath:abcd.xml")// class path
+//@ImportResource("filepath:abcd.xml")//absolute path
 public class AppConfig {
     public AppConfig(){
         System.out.println("AppConfig:Instantiated");
