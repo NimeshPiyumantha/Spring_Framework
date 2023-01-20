e21a86c73310b54e85255268e955cbc28a5e9110
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 09:44:04 +0530

    WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer

--- _09_Spring_Web_MVC/src/lk/ijse/springMVC/WebAppInitializer.java
+++ _09_Spring_Web_MVC/src/lk/ijse/springMVC/WebAppInitializer.java
@@ -1,8 +1,24 @@
 package lk.ijse.springMVC;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
