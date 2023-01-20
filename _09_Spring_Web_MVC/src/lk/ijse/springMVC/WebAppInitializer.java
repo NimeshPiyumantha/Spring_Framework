7e24b6f4694f5f02c52e8848f73a5dc30d53e63e
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 09:39:14 +0530

    Spring web MVC config classes

---
+++ _09_Spring_Web_MVC/src/lk/ijse/springMVC/WebAppInitializer.java
@@ -0,0 +1,8 @@
+package lk.ijse.springMVC;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class WebAppInitializer {
+}
---
+++ _09_Spring_Web_MVC/src/lk/ijse/springMVC/config/WebRootConfig.java
@@ -0,0 +1,8 @@
+package lk.ijse.springMVC.config;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class WebRootConfig {
+}
