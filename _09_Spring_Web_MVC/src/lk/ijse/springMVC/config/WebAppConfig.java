49cf395937dcfc2c244335fc5bebcae09a490e27
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 09:41:23 +0530

    Spring web MVC config to annotations

--- _09_Spring_Web_MVC/src/lk/ijse/springMVC/config/WebAppConfig.java
+++ _09_Spring_Web_MVC/src/lk/ijse/springMVC/config/WebAppConfig.java
@@ -1,8 +1,15 @@
 package lk.ijse.springMVC.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+@Configuration
+@EnableWebMvc
+//@ComponentScan
 public class WebAppConfig {
 }
--- _09_Spring_Web_MVC/src/lk/ijse/springMVC/config/WebRootConfig.java
+++ _09_Spring_Web_MVC/src/lk/ijse/springMVC/config/WebRootConfig.java
@@ -1,8 +1,11 @@
 package lk.ijse.springMVC.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+@Configuration
 public class WebRootConfig {
 }
