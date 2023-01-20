6f8c556aa5ae2f9adbf9d9d4a465ec3617668da3
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 09:34:29 +0530

    Spring web MVC

--- _08_Spring_Environment/src/lk/ijse/spring/AppInitializer.java
+++ _08_Spring_Environment/src/lk/ijse/spring/AppInitializer.java
@@ -3,12 +3,9 @@ package lk.ijse.spring;
 import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.Map;
-import java.util.Properties;
-
 public class AppInitializer {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
