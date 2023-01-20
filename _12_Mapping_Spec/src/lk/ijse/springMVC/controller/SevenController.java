132b1ccabc1a0f6af4d10eb59fca8058d5e93f31
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 16:51:14 +0530

    Interducing http request

---
+++ _12_Mapping_Spec/src/lk/ijse/springMVC/controller/SevenController.java
@@ -0,0 +1,30 @@
+package lk.ijse.springMVC.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@RestController
+@RequestMapping("/seven")
+public class SevenController {
+    public SevenController() {
+        System.out.println("SevenController :Instantiated");
+    }
+    // http request has two parts
+    //Header(meta data)
+    //body(context)
+
+    @GetMapping(consumes = {"application/json"})
+    public String testOne(){
+        return "Invoked method One";
+    }
+    @GetMapping
+    public String testTwo(){
+        return "Invoked method Two";
+    }
+}
