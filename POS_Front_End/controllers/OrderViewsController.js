26d52e53d39bad35c89d38105abded51a9fe9f8b
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-24 20:41:00 +0530

    Add POS_Front_End

---
+++ POS_Front_End/controllers/OrderViewsController.js
@@ -0,0 +1,55 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+let baseUrl = "http://localhost:8080/back_End/";
+loadAllOrders();
+loadAllOrderDetails();
+
+function loadAllOrders() {
+    $("#tblOrder").empty();
+    $.ajax({
+        url: baseUrl + "orders?option=LoadOrders", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+
+            for (let i of res.data) {
+                let orderId = i.orderId;
+                let date = i.date;
+                let cusId = i.cusId;
+
+                let row = "<tr><td>" + orderId + "</td><td>" + date + "</td><td>" + cusId + "</td></tr>";
+                $("#tblOrder").append(row);
+            }
+            console.log(res.message);
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+
+    });
+}
+
+function loadAllOrderDetails() {
+    $("#tblOrderDetails").empty();
+    $.ajax({
+        url: baseUrl + "orders?option=LoadOrderDetails", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+
+            for (let i of res.data) {
+                let OrderId = i.OrderId;
+                let code = i.code;
+                let qty = i.qty;
+                let unitPrice = i.unitPrice;
+
+                let row = "<tr><td>" + OrderId + "</td><td>" + code + "</td><td>" + qty + "</td><td>" + unitPrice + "</td></tr>";
+                $("#tblOrderDetails").append(row);
+            }
+            console.log(res.message);
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+
+    });
+}
