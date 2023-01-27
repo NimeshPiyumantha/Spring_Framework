7143d31060456aae1a28792fe13289d7021dfad6
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-27 14:29:00 +0530

    Update Project

---
+++ _14_Spring_with_Maven/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Configuration
+public class WebRootConfig {
+
+    public WebRootConfig() {
+        System.out.println("WebRootConfig :Instantiated");
+    }
+}
---
+++ _14_Spring_with_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,19 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
+public class CustomerController {
+    public void CallSpring(){
+        System.out.println("Run Call Spring Method.");
+    }
+}
