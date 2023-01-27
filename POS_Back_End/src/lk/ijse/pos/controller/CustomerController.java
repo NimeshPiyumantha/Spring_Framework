62585e5ded25f7cdb42300bcc3faa7b4924e2aab
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-27 12:08:12 +0530

    Introduceing Jackson

--- POS_Back_End/src/lk/ijse/pos/controller/CustomerController.java
+++ POS_Back_End/src/lk/ijse/pos/controller/CustomerController.java
@@ -3,6 +3,8 @@ package lk.ijse.pos.controller;
 import lk.ijse.pos.dto.CustomerDTO;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
@@ -13,7 +15,17 @@ import org.springframework.web.bind.annotation.*;
 public class CustomerController {
 
     @PostMapping
-    public  void saveCustomer(@ModelAttribute CustomerDTO dto){ //@ModelAttribute - not Required
+    public  void saveCustomer(@ModelAttribute CustomerDTO dto){ //@ModelAttribute - not Required annotation
         System.out.println(dto.toString());
+    }
+
+    @PutMapping
+    public ArrayList<CustomerDTO> updateCustomer(@RequestBody CustomerDTO dto) { //@RequestBody - Required annotation
+        ArrayList<CustomerDTO> customerDTOS= new ArrayList<>();
+        customerDTOS.add(dto);
+        customerDTOS.add(dto);
+        customerDTOS.add(dto);
+        customerDTOS.add(dto);
+        return customerDTOS;
     }
 }
