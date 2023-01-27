956a99ee8604106169676e2e2a07f4ea2299b166
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-27 14:41:06 +0530

    Add pom.xl to jackson dependency

--- _14_Spring_with_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ _14_Spring_with_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/customer")
 public class CustomerController {
+    @GetMapping
     public void CallSpring(){
         System.out.println("Run Call Spring Method.");
     }
