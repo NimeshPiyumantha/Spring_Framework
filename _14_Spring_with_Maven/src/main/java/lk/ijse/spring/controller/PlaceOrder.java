2bc4b922114cdb4085d0c30777aa01c99f7698d7
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-31 00:28:06 +0530

    Update Project

--- _14_Spring_with_Maven/src/main/java/lk/ijse/spring/controller/PlaceOrder.java
+++ _14_Spring_with_Maven/src/main/java/lk/ijse/spring/controller/PlaceOrder.java
@@ -1,7 +1,7 @@
 package lk.ijse.spring.controller;
 
-import lk.ijse.spring.dto.CustomerDTO;
-import lk.ijse.spring.dto.ItemDTO;
+import lk.ijse.spring.dto.OrderDTO;
+import lk.ijse.spring.dto.OrderDetailDTO;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,43 @@ import java.util.ArrayList;
 @RequestMapping("/orders")
 public class PlaceOrder {
 
+    ArrayList<OrderDTO> arrayList = new ArrayList<>();
+    ArrayList<OrderDetailDTO> arrayList2 = new ArrayList<>();
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil saveOrder(@RequestBody OrderDTO dto, @RequestBody OrderDetailDTO dto2) { //@ModelAttribute - not Required annotation
+        if (dto.getId().equals("ODI-000")) {
+            throw new RuntimeException("Order Already Exist. Please enter another id..!");
+        }
+        for (OrderDTO orderDTO : arrayList) {
+            if (orderDTO.getId().equals(dto.getId())) {
+                throw new RuntimeException("Order Already Exist. Please enter another id..!");
+            }
+        }
+        arrayList.add(dto);
+        arrayList2.add(dto2);
+        return new ResponseUtil("OK", "Successfully Place Order.!", null);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/LoadOrders")
+    public ResponseUtil LoadOrders() {
+        return new ResponseUtil("OK", "Successfully Loaded. :", arrayList);
+    }
 
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/LoadOrderDetails")
+    public ResponseUtil LoadOrderDetails() {
+        return new ResponseUtil("OK", "Successfully Loaded. :", arrayList2);
+    }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/OrderIdGenerate")
+    public  @ResponseBody String  CustomerIdGenerate() {
+        String iCode= arrayList.get(arrayList.size() - 1).getId();
+        System.out.println(iCode);
+        return iCode;
+    }
 }
