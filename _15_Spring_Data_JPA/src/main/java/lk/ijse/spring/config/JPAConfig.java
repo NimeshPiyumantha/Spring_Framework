7094fe01348a22f095f571f7b94c9ba879ea69e5
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-03 10:14:46 +0530

    Add JPAConfig class

---
+++ _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Configuration
+public class JPAConfig {
+}
--- _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,12 +1,14 @@
 package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 @Configuration
+@Import(JPAConfig.class)
 public class WebRootConfig {
 
     public WebRootConfig() {
