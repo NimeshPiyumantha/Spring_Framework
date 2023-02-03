19d2ed59f4f1cf594eda5d5a3ff317026a05cde3
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-03 09:55:34 +0530

    Update Project

---
+++ _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/entity/Order.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Order {
+    @Id
+    private String id;
+    private String date;
+    private String customerId;
+    private List<OrderDetail> orderDetails;
+
+}
