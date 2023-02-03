c3d97782351d21a0cce5c277ce0b8c12df808e94
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-03 11:16:50 +0530

    Add Customer Save

--- _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +29,9 @@ private CustomerRepo repo;
         if (dto.getId().equals("C00-001")) {
             throw new RuntimeException("Customer Already Exist. Please enter another id..!");
         }
-        for (CustomerDTO customerDTO : arrayList) {
-            if (customerDTO.getId().equals(dto.getId())) {
-                throw new RuntimeException("Customer Already Exist. Please enter another id..!");
-            }
-        }
-        arrayList.add(dto);
+
+        Customer customer =new Customer(dto.getId(),dto.getName(),dto.getAddress(),dto.getSalary());
+        repo.save(customer);
         return new ResponseUtil("OK", "Successfully Registered.!", null);
     }
 
