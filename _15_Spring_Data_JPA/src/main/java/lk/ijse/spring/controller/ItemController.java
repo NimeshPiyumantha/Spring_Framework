5110f158b619ab2ea2dec86e0c1555384eec1a74
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-05 12:25:24 +0530

    Update Project

--- _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/ItemController.java
+++ _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -1,19 +1,11 @@
 package lk.ijse.spring.controller;
 
-import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.ItemDTO;
-import lk.ijse.spring.entity.Customer;
-import lk.ijse.spring.entity.Item;
-import lk.ijse.spring.repo.CustomerRepo;
-import lk.ijse.spring.repo.ItemRepo;
+import lk.ijse.spring.service.ItemService;
 import lk.ijse.spring.util.ResponseUtil;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
@@ -24,64 +16,47 @@ import java.util.ArrayList;
 @CrossOrigin
 @RequestMapping("/item")
 public class ItemController {
-    @Autowired
-    private ItemRepo repo;
 
     @Autowired
-    private ModelMapper mapper;
+    private ItemService service;
+
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseUtil saveItem(@ModelAttribute ItemDTO dto) {
-        if (repo.existsById(dto.getCode())) {
-            throw new RuntimeException("Item Already Exist. Please enter another id..!");
-        }
-        repo.save(mapper.map(dto, Item.class));
+        service.saveItem(dto);
         return new ResponseUtil("OK", "Successfully Registered.!", null);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PutMapping
     public ResponseUtil updateItem(@RequestBody ItemDTO dto) {
-        if (!repo.existsById(dto.getCode())) {
-            throw new RuntimeException("Item Not Exist. Please enter Valid id..!");
-        }
-        repo.save(mapper.map(dto, Item.class));
+        service.updateItem(dto);
         return new ResponseUtil("OK", "Successfully Updated. :" + dto.getCode(), null);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @DeleteMapping
     public ResponseUtil deleteItem(@RequestBody ItemDTO dto) {
-        if (!repo.existsById(dto.getCode())) {
-            throw new RuntimeException("Wrong ID..Please enter valid id..!");
-        }
-        repo.delete(mapper.map(dto, Item.class));
+        service.deleteItem(dto);
         return new ResponseUtil("OK", "Successfully Deleted. :" + dto.getCode(), null);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @GetMapping(path = "/searchItemCode", params = {"code"})
     public ItemDTO searchItemCode(String code) {
-        if (!repo.existsById(code)) {
-            throw new RuntimeException("Wrong ID. Please enter Valid id..!");
-        }
-        Item item = repo.findById(code).get();
-        return mapper.map(item, ItemDTO.class);
+        return service.searchItemCode(code);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @GetMapping(path = "/loadAllItem")
     public ResponseUtil loadAllItem() {
-        ArrayList <ItemDTO> allList= mapper.map(repo.findAll(),new TypeToken<ArrayList<Item>>(){}.getType());
-        return new ResponseUtil("OK", "Successfully Loaded. :",allList);
+        return new ResponseUtil("OK", "Successfully Loaded. :", service.loadAllItem());
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @GetMapping(path = "/ItemIdGenerate")
     public @ResponseBody String ItemIdGenerate() {
-        String code =repo.getLastIndex();
-        System.out.println(code);
-        return code;
+        return service.itemIdGenerate();
     }
 }
