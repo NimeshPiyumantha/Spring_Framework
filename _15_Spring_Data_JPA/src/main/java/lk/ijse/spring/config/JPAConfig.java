f8adfb0cc1a67b7c82c3a4ed558b4aa895aa30ef
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-05 08:10:31 +0530

    Update Project

--- _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ _15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -41,7 +41,7 @@ public class JPAConfig {
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/db?createDatabaseIfNotExist=true");
+        ds.setUrl("jdbc:mysql://localhost:3306/springWebPOSdb?createDatabaseIfNotExist=true");
         ds.setUsername("root");
         ds.setPassword("1234");
         return ds;
